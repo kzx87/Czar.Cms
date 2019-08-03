bfcf423069d4a3f1a0265a42f8628443ef19f342
kzx87 <kanzhixiang@139.com>
2019-08-03 10:44:59 +0800

    新增第五篇文章的示例代码

---
+++ Sample05/Program.cs
@@ -0,0 +1,242 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Sample05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  test_insert();
+
+            test_mult_insert();
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+      static   string sqlconnectionstr = "Data Source=127.0.0.1;User ID=sa;Password=05120512;Initial Catalog=Czar.Cms;Pooling=true;Max Pool Size=100;";
+        /// <summary>
+        /// 测试插入单条数据
+        /// </summary>
+        static void test_insert()
+        {
+            var content = new Content
+            {
+                title = "标题1",
+                content = "内容1",
+
+            };
+            using (var conn = new SqlConnection(sqlconnectionstr))
+            {
+                string sql_insert = @"INSERT INTO [Content]                (title, [content], status, add_time, modify_time) VALUES   (@title,@content,@status,@add_time,@modify_time)";
+                var result = conn.Execute(sql_insert, content);
+                Console.WriteLine($"test_insert：插入了{result}条数据！");
+            }
+        }
+
+        /// <summary>
+        /// 测试一次批量插入两条数据
+        /// </summary>
+        static void test_mult_insert()
+        {
+            List<Content> contents = new List<Content>() {
+               new Content
+            {
+                title = "批量插入标题1",
+                content = "批量插入内容1",
+
+            },
+               new Content
+            {
+                title = "批量插入标题2",
+                content = "批量插入内容2",
+
+            },
+        };
+
+            using (var conn = new SqlConnection(sqlconnectionstr))
+            {
+                string sql_insert = @"INSERT INTO [Content]                (title, [content], status, add_time, modify_time) VALUES   (@title,@content,@status,@add_time,@modify_time)";
+                var result = conn.Execute(sql_insert, contents);
+                Console.WriteLine($"test_mult_insert：插入了{result}条数据！");
+            }
+        }
+
+
+        /// <summary>
+        /// 测试修改单条数据
+        /// </summary>
+        static void test_update()
+        {
+            var content = new Content
+            {
+                id = 5,
+                title = "标题5",
+                content = "内容5",
+
+            };
+            using (var conn = new SqlConnection(sqlconnectionstr))
+            {
+                string sql_insert = @"UPDATE  [Content] SET         title = @title, [content] = @content, modify_time = GETDATE() WHERE   (id = @id)";
+                var result = conn.Execute(sql_insert, content);
+                Console.WriteLine($"test_update：修改了{result}条数据！");
+            }
+        }
+
+        /// <summary>
+        /// 测试一次批量修改多条数据
+        /// </summary>
+        static void test_mult_update()
+        {
+            List<Content> contents = new List<Content>() {
+               new Content
+            {
+                id=6,
+                title = "批量修改标题6",
+                content = "批量修改内容6",
+
+            },
+               new Content
+            {
+                id =7,
+                title = "批量修改标题7",
+                content = "批量修改内容7",
+
+            },
+        };
+
+            using (var conn = new SqlConnection(sqlconnectionstr))
+            {
+                string sql_insert = @"UPDATE  [Content] SET    title = @title, [content] = @content, modify_time = GETDATE() WHERE   (id = @id)";
+                var result = conn.Execute(sql_insert, contents);
+                Console.WriteLine($"test_mult_update：修改了{result}条数据！");
+            }
+        }
+
+        /// <summary>
+        /// 查询单条指定的数据
+        /// </summary>
+        static void test_select_one()
+        {
+            using (var conn = new SqlConnection(sqlconnectionstr))
+            {
+                string sql_insert = @"select * from [dbo].[content] where id=@id";
+                var result = conn.QueryFirstOrDefault<Content>(sql_insert, new { id = 5 });
+                Console.WriteLine($"test_select_one：查到的数据为：");
+            }
+        }
+
+        /// <summary>
+        /// 查询多条指定的数据
+        /// </summary>
+        static void test_select_list()
+        {
+            using (var conn = new SqlConnection(sqlconnectionstr))
+            {
+                string sql_insert = @"select * from [dbo].[content] where id in @ids";
+                var result = conn.Query<Content>(sql_insert, new { ids = new int[] { 6, 7 } });
+                Console.WriteLine($"test_select_one：查到的数据为：");
+            }
+        }
+
+        static void test_select_content_with_comment()
+        {
+            using (var conn = new SqlConnection(sqlconnectionstr))
+            {
+                string sql_insert = @"select * from content where id=@id;
+select * from comment where content_id=@id;";
+                using (var result = conn.QueryMultiple(sql_insert, new { id = 5 }))
+                {
+                    var content = result.ReadFirstOrDefault<ContentWithCommnet>();
+                    content.comments = result.Read<Comment>();
+                    Console.WriteLine($"test_select_content_with_comment:内容5的评论数量{content.comments.Count()}");
+                }
+
+            }
+        }
+
+    }
+
+    public class Content
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int id { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string title { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string content { get; set; }
+        /// <summary>
+        /// 状态 1正常 0删除
+        /// </summary>
+        public int status { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime add_time { get; set; } = DateTime.Now;
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime? modify_time { get; set; }
+    }
+    public class Comment
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// 文章id
+        /// </summary>
+        public int content_id { get; set; }
+        /// <summary>
+        /// 评论内容
+        /// </summary>
+        public string content { get; set; }
+        /// <summary>
+        /// 添加时间
+        /// </summary>
+        public DateTime add_time { get; set; } = DateTime.Now;
+    }
+    public class ContentWithCommnet
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int id { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string title { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string content { get; set; }
+        /// <summary>
+        /// 状态 1正常 0删除
+        /// </summary>
+        public int status { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime add_time { get; set; } = DateTime.Now;
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime? modify_time { get; set; }
+        /// <summary>
+        /// 文章评论
+        /// </summary>
+        public IEnumerable<Comment> comments { get; set; }
+    }
+}
